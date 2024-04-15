f85ba6b1d66548d7585cc58135b6582af4154836
alexis <sawcce@proton.me>
2024-04-15 23:17:45 +0200

    working cwd implementation along task metadata

--- src/taskapp/__init__.py
+++ src/taskapp/__init__.py
@@ -1,19 +1,46 @@
-import pprint
 import subprocess
 import sys
-import inspect
+from typing import Any
+
+current_meta: dict[str, Any] = {}
+
+def get_meta():
+    return current_meta
+
+def get_cwd():
+    return current_meta.get("cwd") 
 
 def run(command: str, *args: str):
-    subprocess.run([command, *args]) 
+    subprocess.run([command, *args], shell=True, cwd=get_cwd())
 
-def task(name: str):
+def task(name: str, dir: str | None = None):
+    """
+        A decorator to define a task, the actual function name is meaningless.  \n
+        name: corresponds to the task name. \n
+        dir: specifies the `cwd` used by taskapp methods (like `run`) when executing the task. \n
+    """
     def wrapper(definition):
         f = list(sys._current_frames().values())[0]
         if f.f_back == None:
             raise Exception("Expected module name")
 
-        module_name = f.f_back.f_globals['__name__']
+        module_name = f.f_back.f_globals["__name__"]
         tn = f"taskapp${module_name}${name}"
-        setattr(sys.modules[module_name], tn, definition)
-        return definition
+
+        meta = {}
+        setattr(sys.modules[module_name], f"{tn}.meta", meta)
+
+        if dir:
+            meta['cwd'] = dir
+
+        def wrapped(*args):
+            old_meta = sys.modules["taskapp"].current_meta
+            sys.modules["taskapp"].current_meta = meta # type: ignore
+            result = definition(*args)
+            sys.modules["taskapp"].current_meta = old_meta # type: ignore
+            return result
+
+        setattr(sys.modules[module_name], tn, wrapped)
+
+        return lambda x: None
     return wrapper
